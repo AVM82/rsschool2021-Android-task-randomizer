050bb34c235ce9e69652dc00491f90b870861d73
avm82 <1982alexvm@gmail.com>
2021-05-29 19:54:21 +0300

    fix: replaced the function of generating a random number

--- app/src/main/java/com/rsschool/android2021/RandomGenerator.kt
+++ app/src/main/java/com/rsschool/android2021/RandomGenerator.kt
@@ -1,13 +1,11 @@
 package com.rsschool.android2021
 
-import kotlin.random.Random
-
 object RandomGenerator {
     private const val min = 0
     private const val max = 255
 
     fun generate(range: Range): Int {
-        return Random.nextInt(range.min, range.max)
+        return IntRange(range.min, range.max).random()
     }
 
     /**
@@ -17,9 +15,6 @@ object RandomGenerator {
         return when {
             range.min > range.max -> {
                 throw IllegalArgumentException("The minimum value cannot be greater than the maximum")
-            }
-            range.min == range.max -> {
-                throw IllegalArgumentException("The minimum value cannot be equal to the maximum value.")
             }
             range.min !in min..max -> {
                 throw IllegalArgumentException("The minimum value is out of range")
